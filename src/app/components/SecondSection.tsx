81defac2370ea16f04e0d547f8b74b1d8df56395
Jaman-Shah <jamanshah5400@gmail.com>
2025-02-25 01:25:13 +0600

    second section overlaping finished

--- src/app/components/SecondSection.tsx
+++ src/app/components/SecondSection.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const SecondSection = () => {
-  return <div className="h-screen bg-gray-600">Section</div>;
+  return (
+    <div className="h-screen bg-gray-600 relative z-50">Second Section</div>
+  );
 };
 
 export default SecondSection;
